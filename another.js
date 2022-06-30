13f14ccc70cd72198bbe4f69de7da3eb156c3256
sweta56 <swetagurung72@gmail.com>
2022-06-30 09:19:44 +0545

    another js

---
+++ another.js
@@ -0,0 +1,26 @@
+const a = 34;
+const b = 3444.33;
+console.log(a===b);
+
+let person = {firstName:"Richa", lastName: "Gurung", age: 22};
+if (person.firstName != "Richa"){
+    console.log("bad person");
+}else{
+    console.log("gooddddd");
+}
+let name = "ola";
+const kalas = 33;
+console.log(`her name is ${name} and she studies in class ${kalas}`); /*this is spread operator*/
+
+const h = 123.456;
+
+const i = parseInt(h);
+console.log(i);
+console.log(typeof(i));
+
+for (let j = 0;j<7;j++){
+    console.log(j);
+}
+const p=32.322;
+const r = 789;
+console.log(p===r);
